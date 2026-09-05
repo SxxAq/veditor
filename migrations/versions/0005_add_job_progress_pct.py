72bb9d6be48c0982e00a2b0b519e8384667e5829
Saalim Aqueel <aqueel192@gmail.com>
2026-09-06 01:17:19 +0530

    Regenerate job progress migration as 0005

--- migrations/versions/0005_add_job_progress_pct.py
+++ migrations/versions/0005_add_job_progress_pct.py
@@ -1,8 +1,8 @@
 """add progress_pct to jobs
 
-Revision ID: 0004
-Revises: 0003
-Create Date: 2026-09-03 12:00:00.000000
+Revision ID: 0005
+Revises: 0004
+Create Date: 2026-09-04 12:00:00.000000
 
 """
 
@@ -12,8 +12,8 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = "0004"
-down_revision: str | Sequence[str] | None = "0003"
+revision: str = "0005"
+down_revision: str | Sequence[str] | None = "0004"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
